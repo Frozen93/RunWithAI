a236acd6fc5cdeae2d094c51f755bb0a3b2ae0be
MariusSuessmilch <47320006+MariusSuessmilch@users.noreply.github.com>
2023-08-25 11:23:13 +0200

    added markdown summary of video

---
+++ src/run_app/text.py
@@ -0,0 +1,19 @@
+texts = {
+    "gym_summary": """
+| Body Part       | Exercise                    | Short Description                               |
+|-----------------|-----------------------------|-------------------------------------------------|
+| **Lower Body**  | **Calf Walks**              | Walking on the balls of the feet.               |
+|                 | **Heel Walks**              | Walking on the heels.                           |
+|                 | **Walking Lunges**          | Stepping forward into a lunge position.         |
+|                 | **Lunge with Dumbbell Reach** | Performing a lunge while holding a dumbbell above the head and reaching down. |
+|                 | **Lunge with Hip Thrust**   | Going into a lunge and then thrusting the hip forward. |
+|                 | **Quarter Squats**          | Performing squats to a quarter depth.           |
+| **Hips & Core** | **Hip Shrugs**              | Standing on one leg and pulling the other leg up using the hip muscle. |
+|                 | **Theraband Exercises**     | Using a theraband for glute activation in various directions. |
+|                 | **Hip Lock and Knee Drive** | Holding a band above the head and driving one leg down towards the knee. |
+|                 | **Long Lever Glute Bridge** | Lying on the back and lifting one leg while keeping the other on the ground. |
+|                 | **Adductor Holds in Side Plank** | Holding a side plank position and working the adductors. |
+| **Flexibility** | **Foam Rolling and Yoga**   | Foam rolling various muscle groups followed by yoga stretches. |
+
+"""
+}
