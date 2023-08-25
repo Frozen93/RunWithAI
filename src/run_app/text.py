f50364842f17213fe5968cb116df9aada0697f50
MariusSuessmilch <47320006+MariusSuessmilch@users.noreply.github.com>
2023-08-25 15:28:57 +0200

    add more exercises

--- src/run_app/text.py
+++ src/run_app/text.py
@@ -14,17 +14,17 @@ texts = {
 | **Flexibility** | [**Hinged Lunge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=9m49s) | Step forward into a lunge, then hinge forward at the hips. | 2-3 sets × 8 reps |
 |                 | [**Lunge > Hip Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=11m34s) | From a lunge position, push your hip forward. | 2-3 sets × 6 reps |
 |                 | [**1/4 Squat w Power Up**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=12m48s) | Perform a shallow squat, then explosively rise. | 2-3 sets × 6 reps |
-| **Hips & Core** | [**Shin Walks**](https://www.youtube.com/watch?v=ANGJPTGREiE) | Walk while emphasizing the shin muscles. | 2-3 sets × 10-15m |
-|                 | [**Bird Dog**](https://www.youtube.com/watch?v=ANGJPTGREiE) | On all fours, extend opposite arm and leg simultaneously. | 2-3 sets × 10-15 reps |
-|                 | [**Dead Bugs**](https://www.youtube.com/watch?v=ANGJPTGREiE) | Lie on back, arms and legs up. Lower opposite arm and leg simultaneously. | 2-3 sets × 10-15 reps |
-|                 | [**Opposite Arm Opposite Leg Push**](https://www.youtube.com/watch?v=ANGJPTGREiE) | Lie on back, push knee against opposite hand for resistance. | 2-3 sets × 10-15 reps |
-| **Flexibility** | [**Kettlebell Lunges**](https://www.youtube.com/watch?v=ANGJPTGREiE) | Lunge while holding kettlebells, keeping chest upright. | 2-3 sets × 8 reps |
-|                 | [**Single Leg RDLs**](https://www.youtube.com/watch?v=ANGJPTGREiE) | Holding a kettlebell, hinge at the hip while balancing on one leg. | 2-3 sets × 8 reps |
-|                 | [**Bosu Balance**](https://www.youtube.com/watch?v=ANGJPTGREiE) | Stand on a Bosu ball, working on balance and stability. | 2-3 sets × 1-2 min |
-|                 | [**Slow Controlled Step Up**](https://www.youtube.com/watch?v=ANGJPTGREiE) | Step up onto a platform slowly, emphasizing control. | 2-3 sets × 8 reps |
-|                 | [**Active Step Up with Knee Drive**](https://www.youtube.com/watch?v=ANGJPTGREiE) | Step up onto a platform and drive the opposite knee up. | 2-3 sets × 8 reps |
-|                 | [**Lunge Back, Hip Drive to Box**](https://www.youtube.com/watch?v=ANGJPTGREiE) | Lunge backward, then drive the hip forward onto a box. | 2-3 sets × 8 reps |
-|                 | [**Kettlebell Squat**](https://www.youtube.com/watch?v=ANGJPTGREiE) | Perform a squat while holding a kettlebell, keeping knees outward. | 2-3 sets × 8 reps |
+| **Lower Body**  | [**Shin Walks**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=1m10s) | Walk while emphasizing the shin muscles. | 2-3 sets × 10-15m |
+| **Hips & Core** | [**Bird Dog**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=1m10s) | On all fours, extend opposite arm and leg simultaneously. | 2-3 sets × 10-15 reps |
+|                 | [**Dead Bugs**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=1m10s) | Lie on back, arms and legs up. Lower opposite arm and leg simultaneously. | 2-3 sets × 10-15 reps |
+|                 | [**Opposite Arm Opposite Leg Push**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=1m10s) | Lie on back, push knee against opposite hand for resistance. | 2-3 sets × 10-15 reps |
+| **Flexibility** | [**Kettlebell Lunges**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=1m10s) | Lunge while holding kettlebells, keeping chest upright. | 2-3 sets × 8 reps |
+|                 | [**Single Leg RDLs**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=1m10s) | Holding a kettlebell, hinge at the hip while balancing on one leg. | 2-3 sets × 8 reps |
+| **Balance**     | [**Bosu Balance**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=1m10s) | Stand on a Bosu ball, working on balance and stability. | 2-3 sets × 1-2 min |
+| **Flexibility** | [**Slow Controlled Step Up**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=1m10s) | Step up onto a platform slowly, emphasizing control. | 2-3 sets × 8 reps |
+|                 | [**Active Step Up with Knee Drive**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=1m10s) | Step up onto a platform and drive the opposite knee up. | 2-3 sets × 8 reps |
+|                 | [**Lunge Back, Hip Drive to Box**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=1m10s) | Lunge backward, then drive the hip forward onto a box. | 2-3 sets × 8 reps |
+|                 | [**Kettlebell Squat**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=1m10s) | Perform a squat while holding a kettlebell. | 2-3 sets × 8 reps |
 
 """
 }
