81e2cbd97da3548b2b906434f43d045454816009
MariusSuessmilch <47320006+MariusSuessmilch@users.noreply.github.com>
2023-08-25 15:57:06 +0200

    add more exercises

--- src/run_app/text.py
+++ src/run_app/text.py
@@ -11,13 +11,13 @@ texts = {
 |                 | [**Hip Lock and Knee Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=6m30s) | Stand with resistance band anchored at ankle. Drive banded knee up towards chest. | 2-3 sets × 8 reps |
 |                 | [**Long Lever Glute Bridge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=7m20s) | Lie on your back and extend one leg straight while keeping the other foot on the ground. | 2-3 sets × 5-8 sec |
 |                 | [**Side Plank Abductor Hold**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=8m2s) | Assume a side plank position and lift the top leg, working the outer thigh and core muscles. | 2-3 sets × 10-20 sec |
+|                 | [**Bird Dog**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=4m21s) | On all fours, extend opposite arm and leg simultaneously. | 2-3 sets × 10 reps |
+|                 | [**Dead Bugs**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=4m36s) | Lie on back, arms and legs up. Lower opposite arm and leg simultaneously. | 2-3 sets × 10 reps |
+|                 | [**Opposite Arm Opposite Leg Push**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=4m48s) | Lie on back, push knee against opposite hand for resistance. | 2-3 sets × 10-15 reps |
 | **Flexibility** | [**Hinged Lunge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=9m49s) | Step forward into a lunge, then hinge forward at the hips. | 2-3 sets × 8 reps |
 |                 | [**Lunge > Hip Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=11m34s) | From a lunge position, push your hip forward. | 2-3 sets × 6 reps |
 |                 | [**1/4 Squat w Power Up**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=12m48s) | Perform a shallow squat, then explosively rise. | 2-3 sets × 6 reps |
-| **Hips & Core** | [**Bird Dog**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=4m21s) | On all fours, extend opposite arm and leg simultaneously. | 2-3 sets × 10 reps |
-|                 | [**Dead Bugs**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=4m36s) | Lie on back, arms and legs up. Lower opposite arm and leg simultaneously. | 2-3 sets × 10 reps |
-|                 | [**Opposite Arm Opposite Leg Push**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=4m48s) | Lie on back, push knee against opposite hand for resistance. | 2-3 sets × 10-15 reps |
-| **Flexibility** | [**Kettlebell Lunges**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=5m6s) | Lunge while holding kettlebells, keeping chest upright. | 2-3 sets × 8 reps |
+|                 | [**Kettlebell Lunges**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=5m6s) | Lunge while holding kettlebells, keeping chest upright. | 2-3 sets × 8 reps |
 |                 | [**Single Leg RDLs**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=5m51s) | Holding a kettlebell, hinge at the hip while balancing on one leg. | 2-3 sets × 8 reps |
 | **Balance**     | [**Bosu Balance**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=6m23s) | Stand on a Bosu ball, working on balance and stability. | 2-3 sets × 15-20 sec |
 | **Flexibility** | [**Slow Controlled Step Up**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=6m40s) | Step up onto a platform slowly, emphasizing control. | 2-3 sets × 8 reps |
