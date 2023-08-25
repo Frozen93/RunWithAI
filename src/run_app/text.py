0338fd7e80295591b2db6c15778d195f32a3481d
MariusSuessmilch <47320006+MariusSuessmilch@users.noreply.github.com>
2023-08-25 11:31:04 +0200

    added markdown summary of video

--- src/run_app/text.py
+++ src/run_app/text.py
@@ -1,20 +1,19 @@
 texts = {
     "gym_summary": """
-| Body Part       | Exercise                    | Short Description                               |
-|-----------------|-----------------------------|-------------------------------------------------|
-| **General**     | [**Gym program**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=1m38s) | 2-3x per week. |
-| **Lower Body**  | [**Calf Walks**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=1m41s) | Walking on the balls of the feet. 2-3 × 15m. |
-|                 | [**Active Toe Plants**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=1m53s) | Stay tall. |
-|                 | [**Heel Walks**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=2m57s) | Walking on the heels. 2-3 × 15m. |
-|                 | [**Lunge Circuit**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=3m39s) | 1× 5-6 each side. Drive knee up when coming up and lift med ball above head. |
-| **Hips & Core** | [**Hip Raises**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=3m12s) | 2-3 × 6-8 reps. |
-|                 | [**Theraband**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=5m28s) | 2-3 × 6-8 per position. Standing straight leg, left and right, diagonals, straight back. |
-|                 | [**Hip Lock and Knee Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=6m30s) | 2-3 × 8. |
-|                 | [**Long Lever Glute Bridge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=7m20s) | Lying on the back and lifting one leg. 2-3 × 5-8 sec. |
-|                 | [**Side Plank Abductor Hold**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=8m2s) | Holding a side plank position and working the adductors. 2-3 × 10-20 sec. |
-| **Flexibility** | [**Hinged Lunge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=9m49s) | 2-3 × 8. |
-|                 | [**Lunge > Hip Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=11m34s) | Going into a lunge and then thrusting the hip forward. 2-3 × 6. |
-|                 | [**1/4 Squat w Power Up**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=12m48s) | Performing squats to a quarter depth with power up. 2-3 × 6. |
+| Body Part       | Exercise                    | Short Description                               | Repetitions/Durations |
+|-----------------|-----------------------------|-------------------------------------------------|-----------------------|
+| **Lower Body**  | [**Calf Walks**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=1m41s) | Walking on the balls of the feet. | 2-3 × 15m |
+|                 | [**Active Toe Plants**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=1m53s) | Stay tall. | - |
+|                 | [**Heel Walks**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=2m57s) | Walking on the heels. | 2-3 × 15m |
+|                 | [**Lunge Circuit**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=3m39s) | 1× 5-6 each side. Drive knee up when coming up and lift med ball above head. | 1× 5-6 each side |
+| **Hips & Core** | [**Hip Raises**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=3m12s) | - | 2-3 × 6-8 reps |
+|                 | [**Theraband**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=5m28s) | 2-3 × 6-8 per position. Standing straight leg, left and right, diagonals, straight back. | 2-3 × 6-8 per position |
+|                 | [**Hip Lock and Knee Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=6m30s) | - | 2-3 × 8 |
+|                 | [**Long Lever Glute Bridge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=7m20s) | Lying on the back and lifting one leg. | 2-3 × 5-8 sec |
+|                 | [**Side Plank Abductor Hold**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=8m2s) | Holding a side plank position and working the adductors. | 2-3 × 10-20 sec |
+| **Flexibility** | [**Hinged Lunge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=9m49s) | - | 2-3 × 8 |
+|                 | [**Lunge > Hip Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=11m34s) | Going into a lunge and then thrusting the hip forward. | 2-3 × 6 |
+|                 | [**1/4 Squat w Power Up**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=12m48s) | Performing squats to a quarter depth with power up. | 2-3 × 6 |
 
 """
 }
