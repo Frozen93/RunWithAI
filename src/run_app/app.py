e17b0432b7968456d7d20bb9b5d8f9c12fc99e47
MariusSuessmilch <47320006+MariusSuessmilch@users.noreply.github.com>
2023-08-25 19:56:25 +0200

    add fatigue score

--- src/run_app/app.py
+++ src/run_app/app.py
@@ -125,7 +125,8 @@ def display_comparison_metrics(df: pd.DataFrame, df_raw: pd.DataFrame):
         st.subheader("Fatigue Score")
         st.markdown(
             """
-**0-30% Fatigue:** Within a safe training range; proceed as planned but.
+**0-30% Fatigue:** Within a safe training range; proceed as planned but.  
+
 **30-50% Fatigue:** Be careful. If this persists, re-evaluate your routine for more balance.
 
 **50%+ Fatigue:** High overtraining risk. Prioritize rest, sleep, and nutrition.
