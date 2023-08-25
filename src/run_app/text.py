ca741edfef9832a9f3a2985feca1bb3ae51a955d
MariusSuessmilch <47320006+MariusSuessmilch@users.noreply.github.com>
2023-08-25 11:45:14 +0200

    added markdown summary of video

--- src/run_app/text.py
+++ src/run_app/text.py
@@ -2,18 +2,17 @@ texts = {
     "gym_summary": """
 | Body Part       | Exercise                    | Short Description                               | Repetitions/Durations |
 |-----------------|-----------------------------|-------------------------------------------------|-----------------------|
-| **Lower Body**  | [**Calf Walks**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=1m41s) | Walking on the balls of the feet. | 2-3 × 10-15m |
-|                 | [**Active Toe Plants**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=1m53s) | Stay tall. | 2-3 × 10-15m |
-|                 | [**Heel Walks**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=2m57s) | Walking on the heels. | 2-3 × 10-15m |
-|                 | [**Lunge Circuit**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=3m39s) | 1× 5-6 each side. Drive knee up when coming up and lift med ball above head. | 1× 5-6 each side |
+| **Lower Body**  | [**Calf Walks**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=1m41s) | Walk on the balls of your feet, emphasizing calf engagement. | 2-3 × 10-15m |
+|                 | [**Active Toe Plants**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=1m53s) | While standing tall, actively plant your toes on the ground. | 2-3 × 10-15m |
+|                 | [**Heel Walks**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=2m57s) | Walk solely on your heels, keeping the balls of your feet elevated. | 2-3 × 10-15m |
+|                 | [**Lunge Circuit**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=3m39s) | Perform a series of lunges, incorporating variations using a medicine ball. | 1× 5-6 each side |
 | **Hips & Core** | [**Hip Raises**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=3m12s) | Lie on back, knees bent. Lift hips to form a straight line from shoulders to knees. | 2-3 × 6-8 reps |
-|                 | [**Theraband**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=5m28s) | 2-3 × 6-8 per position. Standing straight leg, left and right, diagonals, straight back. | 2-3 × 6-8 per position |
-|                 | [**Hip Lock and Knee Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=6m30s) | Stand with resistance band anchored at ankle. Drive banded knee up towards chest. | 2-3 × 8 |
-|                 | [**Long Lever Glute Bridge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=7m20s) | Lying on the back and lifting one leg. | 2-3 × 5-8 sec |
-|                 | [**Side Plank Abductor Hold**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=8m2s) | Holding a side plank position and working the adductors. | 2-3 × 10-20 sec |
-| **Flexibility** | [**Hinged Lunge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=9m49s) | Step into a lunge, hinge forward at hips. Return to start. | 2-3 × 8 |
-|                 | [**Lunge > Hip Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=11m34s) | Going into a lunge and then thrusting the hip forward. | 2-3 × 6 |
-|                 | [**1/4 Squat w Power Up**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=12m48s) | Performing squats to a quarter depth with power up. | 2-3 × 6 |
-
+|                 | [**Theraband**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=5m28s) | Execute a series of leg exercises using a resistance band. | 2-3 × 6-8 reps per position |
+|                 | [**Hip Lock and Knee Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=6m30s) | Stand with resistance band anchored at ankle. Drive banded knee up towards chest. | 2-3 × 8 reps |
+|                 | [**Long Lever Glute Bridge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=7m20s) | Lie on your back and extend one leg straight while keeping the other foot on the ground. | 2-3 × 5-8 sec |
+|                 | [**Side Plank Abductor Hold**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=8m2s) | Assume a side plank position and lift the top leg, working the outer thigh and core muscles. | 2-3 × 10-20 sec |
+| **Flexibility** | [**Hinged Lunge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=9m49s) | Step forward into a lunge, then hinge forward at the hips. | 2-3 × 8 reps |
+|                 | [**Lunge > Hip Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=11m34s) | From a lunge position, powerfully thrust your hip forward. | 2-3 × 6 reps |
+|                 | [**1/4 Squat w Power Up**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=12m48s) | Perform a shallow squat, then explosively rise. | 2-3 × 6 reps |
 """
 }
