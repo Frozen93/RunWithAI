d3b534f47ba04122aa0f4112f540ee30d7a1d5c3
MariusSuessmilch <47320006+MariusSuessmilch@users.noreply.github.com>
2023-08-26 11:38:09 +0200

    add fatigue score

--- src/run_app/text.py
+++ src/run_app/text.py
@@ -14,16 +14,16 @@ texts = {
 |                 | [**Bird Dog**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=4m21s) | On all fours, extend opposite arm and leg simultaneously. | 2-3 sets × 10 reps |
 |                 | [**Dead Bugs**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=4m36s) | Lie on back, arms and legs up. Lower opposite arm and leg simultaneously. | 2-3 sets × 10 reps |
 |                 | [**Opposite Arm Opposite Leg Push**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=4m48s) | Lie on back, push knee against opposite hand for resistance. | 2-3 sets × 10-15 reps |
-| **Flexibility** | [**Hinged Lunge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=9m49s) | Step forward into a lunge, then hinge forward at the hips. | 2-3 sets × 8 reps |
-|                 | [**Lunge > Hip Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=11m34s) | From a lunge position, push your hip forward. | 2-3 sets × 6 reps |
+| **Flexibility** | [**Hinged Lunge**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=9m49s) | Step forward into a lunge, then hinge forward at the hips. | 2-3 sets × 8 reps per side |
+|                 | [**Lunge > Hip Drive**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=11m34s) | From a lunge position, push your hip forward. | 2-3 sets × 6 reps per side |
 |                 | [**1/4 Squat w Power Up**](https://www.youtube.com/watch?v=SnNlrFcXjVU&t=12m48s) | Perform a shallow squat, then explosively rise. | 2-3 sets × 6 reps |
-|                 | [**Kettlebell Lunges**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=5m6s) | Lunge while holding kettlebells, keeping chest upright. | 2-3 sets × 8 reps |
-|                 | [**Single Leg RDLs**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=5m51s) | Holding a kettlebell, hinge at the hip while balancing on one leg. | 2-3 sets × 8 reps |
+|                 | [**Kettlebell Lunges**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=5m6s) | Lunge while holding kettlebells, keeping chest upright. | 2-3 sets × 10 reps |
+|                 | [**Single Leg RDLs**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=5m51s) | Holding a kettlebell, hinge at the hip while balancing on one leg. | 2-3 sets × 4-5 reps |
 | **Balance**     | [**Bosu Balance**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=6m23s) | Stand on a Bosu ball, working on balance and stability. | 2-3 sets × 15-20 sec |
-| **Flexibility** | [**Slow Controlled Step Up**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=6m40s) | Step up onto a platform slowly, emphasizing control. | 2-3 sets × 8 reps |
-|                 | [**Active Step Up with Knee Drive**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=6m54s) | Step up onto a platform and drive the opposite knee up. | 2-3 sets × 8 reps |
-|                 | [**Lunge to Hip Drive**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=7m19s) | Lunge backward, then drive the hip forward onto a box. | 2-3 sets × 8 reps |
-|                 | [**Kettlebell Squat**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=7m47s) | Perform a squat while holding a kettlebell. | 2-3 sets × 8 reps |
+| **Flexibility** | [**Slow Controlled Step Up**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=6m40s) | Step up onto a platform slowly, emphasizing control. | 2-3 sets × 15-20 sec |
+|                 | [**Active Step Up with Knee Drive**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=6m54s) | Step up onto a platform and drive the opposite knee up. | 2-3 sets × 15-20 sec per side |
+|                 | [**Lunge to Hip Drive**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=7m19s) | Lunge backward, then drive the hip forward onto a box. | 2-3 sets × 8 15-20 sec per side |
+|                 | [**Kettlebell Squat**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=7m47s) | Perform a squat while holding a kettlebell. | 2-3 sets × 10-15 reps |
 | **Recovery**    | [**Foam Rolling**](https://www.youtube.com/watch?v=ANGJPTGREiE&t=2m15s) | Use a foam roller to massage and release muscle tightness. | 1-2 sets 20-30 secs |
 """
 }
