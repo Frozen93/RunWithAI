f25ffe4a23a79e7c1c0fcf9bb61af753f1ba454a
MariusSuessmilch <47320006+MariusSuessmilch@users.noreply.github.com>
2023-08-28 15:59:17 +0200

    fatigue error handling

--- src/run_app/app.py
+++ src/run_app/app.py
@@ -397,7 +397,7 @@ def main():
             "suffer_score",
         ]
 
-        plots.plot_scatter_metrics_with_regression(df, metrics_list)
+        # plots.plot_scatter_metrics_with_regression(df, metrics_list)
 
         a, _, b = st.columns((6, 1, 6))
         with a:
